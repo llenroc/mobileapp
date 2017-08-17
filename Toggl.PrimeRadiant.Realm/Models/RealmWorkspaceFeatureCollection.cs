c3c2cb7a6afb59055b8c1ec04ad96b998cb44094
Simon Rozsival <simon@rozsival.com>
2017-08-17 11:50:02 +0200

    :sparkles: Add realm models for workspace features

---
+++ Toggl.PrimeRadiant.Realm/Models/RealmWorkspaceFeatureCollection.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Realms;
+using Toggl.Multivac;
+using Toggl.Multivac.Models;
+using Toggl.PrimeRadiant.Models;
+
+namespace Toggl.PrimeRadiant.Realm
+{
+    internal class RealmWorkspaceFeatureCollection : RealmObject, IDatabaseWorkspaceFeatureCollection
+    {
+        public long WorkspaceId { get; set; }
+
+        public IList<RealmWorkspaceFeature> RealmWorkspaceFeatures { get; }
+
+        public IEnumerable<IDatabaseWorkspaceFeature> DatabaseFeatures
+            => RealmWorkspaceFeatures?.Cast<IDatabaseWorkspaceFeature>().ToList() ?? new List<IDatabaseWorkspaceFeature>();
+
+        public IEnumerable<IWorkspaceFeature> Features
+            => DatabaseFeatures.ToList<IWorkspaceFeature>();
+
+        public bool IsEnabled(WorkspaceFeatureId feature)
+            => Features.Any(f => f.FeatureId == feature && f.Enabled);
+    }
+}
