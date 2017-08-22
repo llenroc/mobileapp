3651f4c200655e7edcf26dd94c5059f1cced9c7b
Simon Rozsival <simon@rozsival.com>
2017-08-22 13:41:50 +0200

    :construction: Create IBaseRepository interface

---
+++ Toggl.PrimeRadiant/IBaseRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reactive;
+
+namespace Toggl.PrimeRadiant
+{
+    public interface IBaseRepository<TModel>
+    {
+        IObservable<TModel> Create(TModel entity);
+        IObservable<TModel> Update(TModel entity);
+        IObservable<Unit> Delete(TModel entity);
+        IObservable<IEnumerable<TModel>> GetAll();
+        IObservable<IEnumerable<TModel>> GetAll(Func<TModel, bool> predicate);
+    }
+}
--- Toggl.PrimeRadiant/IRepository.cs
+++ Toggl.PrimeRadiant/IRepository.cs
@@ -1,18 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Reactive;
 using Toggl.Multivac.Models;
 
 namespace Toggl.PrimeRadiant
 {
-    public interface IRepository<TModel>
+    public interface IRepository<TModel> : IBaseRepository<TModel>
         where TModel : IBaseModel, IDatabaseSyncable
     {
         IObservable<TModel> GetById(long id);
-        IObservable<TModel> Create(TModel entity);
-        IObservable<TModel> Update(TModel entity);
-        IObservable<Unit> Delete(TModel entity);
-        IObservable<IEnumerable<TModel>> GetAll();
-        IObservable<IEnumerable<TModel>> GetAll(Func<TModel, bool> predicate);
     }
 }
