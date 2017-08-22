3e0a839f005376df6eab61925c463f2faf49d262
Simon Rozsival <simon@rozsival.com>
2017-08-22 14:56:03 +0200

    :construction: Split repository tests

---
+++ Toggl.PrimeRadiant.Tests/BaseRepositoryTests.cs
@@ -0,0 +1,47 @@
+﻿﻿using System;
+using System.Linq;
+using System.Reactive;
+using System.Reactive.Linq;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Toggl.Multivac.Extensions;
+using Xunit;
+
+namespace Toggl.PrimeRadiant.Tests
+{
+    public abstract class BaseRepositoryTests<TTestModel> : BaseStorageTests<TTestModel>
+        where TTestModel : class, IDatabaseSyncable, new()
+    {
+        protected sealed override IObservable<TTestModel> Create(TTestModel testModel)
+            => BaseRepository.Create(testModel);
+
+        protected sealed override IObservable<TTestModel> Update(TTestModel testModel)
+            => BaseRepository.Update(testModel);
+
+        protected sealed override IObservable<Unit> Delete(TTestModel testModel)
+            => BaseRepository.Delete(testModel);
+
+        protected abstract IBaseRepository<TTestModel> BaseRepository { get; }
+
+        [Fact]
+        public async Task TheGetAllMethodReturnsAnEmptyListIfThereIsNothingOnTheRepository()
+        {
+            var entities = await BaseRepository.GetAll(_ => true);
+            entities.Count().Should().Be(0);
+        }
+
+        [Fact]
+        public async Task TheGetAllMethodReturnsAllItemsThatMatchTheQuery()
+        {
+            const int numberOfItems = 5;
+
+            await Enumerable.Range(0, numberOfItems)
+                            .Select(GetModelWith)
+                            .Select(async model => await BaseRepository.Create(model))
+                            .Apply(Task.WhenAll);
+
+            var entities = await BaseRepository.GetAll(_ => true);
+            entities.Count().Should().Be(numberOfItems);
+        }
+    }
+}
--- Toggl.PrimeRadiant.Tests/RepositoryTests.cs
+++ Toggl.PrimeRadiant.Tests/RepositoryTests.cs
@@ -1,28 +1,18 @@
 ﻿﻿using System;
-using System.Linq;
-using System.Reactive;
 using System.Reactive.Linq;
 using System.Threading.Tasks;
 using FluentAssertions;
 using Toggl.PrimeRadiant.Exceptions;
-using Toggl.Multivac.Extensions;
 using Xunit;
 
 namespace Toggl.PrimeRadiant.Tests
 {
-    public abstract class RepositoryTests<TTestModel> : BaseStorageTests<TTestModel>
+    public abstract class RepositoryTests<TTestModel> : BaseRepositoryTests<TTestModel>
         where TTestModel : class, ITestModel, IDatabaseSyncable, new()
     {
-        protected sealed override IObservable<TTestModel> Create(TTestModel testModel)
-            => Repository.Create(testModel);
-
-        protected sealed override IObservable<TTestModel> Update(TTestModel testModel)
-            => Repository.Update(testModel);
-
-        protected sealed override IObservable<Unit> Delete(TTestModel testModel)
-            => Repository.Delete(testModel);
-
         protected abstract IRepository<TTestModel> Repository { get; }
+
+        protected override IBaseRepository<TTestModel> BaseRepository => Repository;
 
         [Fact]
         public void TheGetByIdMethodThrowsIfThereIsNoEntityWithThatIdInTheRepository()
@@ -42,27 +32,6 @@ namespace Toggl.PrimeRadiant.Tests
 
             var element = await Repository.GetById(testEntity.Id).SingleAsync();
             element.Should().Be(testEntity);
-        }
-
-        [Fact]
-        public async Task TheGetAllMethodReturnsAnEmptyListIfThereIsNothingOnTheRepository()
-        {
-            var entities = await Repository.GetAll(_ => true);
-            entities.Count().Should().Be(0);
-        }
-
-        [Fact]
-        public async Task TheGetAllMethodReturnsAllItemsThatMatchTheQuery()
-        {
-            const int numberOfItems = 5;
-
-            await Enumerable.Range(0, numberOfItems)
-                            .Select(GetModelWith)
-                            .Select(async model => await Repository.Create(model))
-                            .Apply(Task.WhenAll);
-            
-            var entities = await Repository.GetAll(_ => true);
-            entities.Count().Should().Be(numberOfItems);
         }
     }
 }
